8e5cd3c28547aaeda3ee5e841b780467872f3576
NurMohammad56 <milonsardar56@gmail.com>
2025-07-13 23:27:37 +0600

    test

---
+++ day-3/day3.js
@@ -0,0 +1,9 @@
+"use strict";
+// let greetings: string = "Nur Mohammad";
+Object.defineProperty(exports, "__esModule", { value: true });
+// greetings.toUpperCase();
+// let myNum: number = 5;
+// myNum.toFixed();
+// console.log(greetings);
+var isBoolean = true;
+isBoolean.valueOf();
---
+++ day-3/day3.ts
@@ -0,0 +1,15 @@
+// let greetings: string = "Nur Mohammad";
+
+// greetings.toUpperCase();
+
+// let myNum: number = 5;
+
+// myNum.toFixed();
+
+// console.log(greetings);
+
+// let isBoolean: boolean = true;
+
+// isBoolean.valueOf();
+
+export {};
