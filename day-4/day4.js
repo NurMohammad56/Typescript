60d20f34281f7a3c04d1537d6a21293d3fa75ca8
NUR MOHAMMAD <116737444+NurMohammad56@users.noreply.github.com>
2026-06-07 15:47:55 +0600

    Remove faaaaaaaaaaaa

--- day-4/day4.js
+++ day-4/day4.js
@@ -1,9 +1,3 @@
-"use strict";
-// number
-// let userId: number = 34784.43;
-Object.defineProperty(exports, "__esModule", { value: true });
-// boolean
-// let isLoggedIn: boolean = true;
 function addTwo(num) {
     return num + 2;
 }
@@ -17,6 +11,7 @@ function signUpUser(name, email, password, isPaid) {
 var loginUser = function (name, password, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
+
 addTwo(5);
 getUpper("D5");
 signUpUser("John Doe", "john@example.com", "password123", true);
